92216962fc65e2f4021f7f923c9f6fa77e6127de
hibbardr <59662877+hibbardr@users.noreply.github.com>
2023-03-17 16:33:35 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.57.1"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = "ASIA35KEIHUNNMLXEU4C"
+  secret_key = "K0gkMAeu4zNiCvoWuzQA1g6OalYzr9GRSMkpvM48"
+  token      = "FwoGZXIvYXdzEH0aDH1ZE49reCU6HxKwoyLJASI6ggQBTCwhv85zTo1Eyd2hSeOdm07QDrw4mRlp1rsSIdip2saJ9A1mCxRKupYDD1K/FP4GhDRrLk+N94L17DvWEJAEH9pOvG3fe6HVNDgOj1DV2kmZVAGY3Teno0NDJKwPJAc7Z3prDLn8cUb2jz/vTBSxR0ahMVWc9Pmnc6Bgk2vumsB/C9Oi2izBllDA+jk8+rthqj2Tpq9P1MWiRxbmEyz2cU6nmxf3YdJHAvGaCIOe8ugfwWbJY4WrQpyzUy9HhIVumwB4xCjH9NKgBjIt2xf33iIDTMcEuzRPJuXJrnpuPLpwU7NdzaOtriZDYELgMek9kNw/sV8L77zI"
+}
